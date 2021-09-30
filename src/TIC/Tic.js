8f54325353430ac387d4f21b54032044f3af4f1a
root <dijayst@gmail.com>
2021-09-30 05:46:21 +0100

    hooks tictactoes

--- src/TIC/Tic.js
+++ src/TIC/Tic.js
@@ -1,13 +1,78 @@
-import React, { Component } from 'react'
+import { useState } from "react";
 
-export class Tic extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
+
+const  cells=["","","","","","","b","",""];
+
+
+/*const AddIcon = "X"
+const RemoveIcon = "O";*/
+
+
+
+
+export default function ListItem({ item }) {
+  
+  const [checked, setChecked] = useState(false);
+
+
+  /*
+useEffect(() => {
+const winner= checkwinner();
+if(winner){alert(`you won!${winner}`)
+cells
+}
+  return () => {
+    cleanup
   }
+}, [cells])
+
+const refresh=()=>{
+
+}
+*/
+  const checkwinner=()=>{
+    const pattern=[
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+     for(let i=0; i<pattern.length;i++){
+       console.log(pattern[i]);
+      const[a,b,c]=pattern[i];
+      if(cells[a]===cells[b]===cells[c]){
+        return cells[a];
+      }
+    }
+    return null;
+  }
+
+
+
+
+  return (<div>
+    <li
+      onClick={() => {
+        setChecked(!checked);
+        console.log(checked)
+      }}
+    >{checked ? "x" : "o" }
+      
+    </li>
+
+
+<ul className="Appdiv">
+{cells.map((item,index) => {
+  return <ListItem item={item}key={item} />;
+})}
+</ul>
+<button>{cells}</button>
+</div>
+  );
 }
 
-export default Tic
+
+
+
+
--- src/TIC/Tictactoe.js
+++ src/TIC/Tictactoe.js
@@ -14,76 +14,129 @@ console.log(this.state.cells)
 }
 /*
 handleclick=(i)=>{
-  let square=this.state.checked;
-if(this.state.cells){
-  square[i]="X";
-  this.setState((state)=>{const cells=state.cells.map(item=>"X")
-  return{cells}})
-  console.log(this.state.cells)
-}
-else{ square[i]="O";
-this.setState((state)=>{const cells=state.cells.map(item=>"O")
-return{cells}})
-console.log(this.state.cells)
-}
-this.setState({checked:square})
-console.log(square)
-
-}*/
-/*
-handleclick=(i)=>{
   let square=this.state.cells;
 if(this.state.checked==="X"){
-square[i]="X";
+square[1]="X";
 this.setState({checked:"O"})
 }else{
- square[i]="O";
+ square[1]="O";
 this.setState({checked:"X"})
 }
 this.setState({cells:square})
 console.log(square)
 return(square)
+}*/
+/*
+handleclick=(i)=>{
+ let square=this.state.cells[i];
+  switch (square) {
+    case this.state.cells[i]:
+      if(this.state.checked==="X"){
+        this.setState((stat)=>{const cells=stat.cells.map(item=>"X")
+        return{cells}})
+        console.log(this.state.cells[1])
+      }
+      else{ 
+      this.setState((stat)=>{const cells=stat.cells.map(item=>"O")
+      return{cells}})
+      console.log(this.state.cells)
+      }
+      break;
+      case this.state.cells[1]:
+        if(this.state.checked==="X"){
+          this.setState((stat)=>{const cells=stat.cells.map(item=>"Y")
+          return{cells}})
+          console.log(this.state.cells)
+        }
+        else{ 
+        this.setState((stat)=>{const cells=stat.cells.map(item=>"M")
+        return{cells}})
+        console.log(this.state.cells)
+        }
+      break;
+    
+      case this.state.cells[2]:
+        if(this.state.checked==="X"){
+          this.setState((stat)=>{const cells=stat.cells.map(item=>"V")
+          return{cells}})
+          console.log(this.state.cells)
+        }
+        else{ 
+        this.setState((stat)=>{const cells=stat.cells.map(item=>"W")
+        return{cells}})
+        console.log(this.state.cells)
+        }
+      break;
+    default:
+     this.setState({checked:"y"})
+      break;
+  }
+  
 }
 */
-handleclick=(i)=>{
+handleclick=()=>{
   let square=this.state.checked;
 if(this.state.checked==="X"){
-  this.setState((state)=>{const cells=state.cells.map(item=>"X")
+  this.setState((stat)=>{const cells=stat.cells.map(item=>"X")
   this.setState({checked:"O"})
   return{cells}})
   console.log(this.state.cells)
 }
 else{ 
-this.setState((state)=>{const cells=state.cells.map(item=>"O")
+this.setState((stat)=>{const cells=stat.cells.map(item=>"O")
 this.setState({checked:"X"})
 return{cells}})
 console.log(this.state.cells)
 }
 this.setState({checked:square})
 console.log(square)
-
 }
-/*
-handleclick=(e)=>{
-  const newcells=this.state.cells.slice()
-  newcells[0]="p";
-  newcells[1]="y";
-  newcells[2]="y"
-  newcells[3]="y"
-  newcells[4]="y"
-  newcells[5]="y"
-  newcells[6]="y"
-  newcells[7]="y"
-  newcells[8]="y"
- }
-/*
-handleclick=(new,i)=>{
-  mas=this.state.cells;
- mas[i]=new;
- setstate({cells:mas})
-}
- */
 
+/*
+handleclick=(i)=>{
+let squ=this.state.cells;
+squ[1]="yess";
+this.setState({cells:squ})
+
+squ[2]="no";
+this.setState({cells:squ})
+console.log(this.setState.cells)
+console.log(this.state.cells)
+}
+*//*
+handleclick=(i)=>{
+  let squ=this.state.cells;
+  switch (squ) {
+    case squ[1]:
+  if(this.state.checked==="X"){
+    squ[1]="X";
+    this.setState({checked:"O"})
+  }  else{
+    squ[1]="O";
+  this.setState({checked:"X"})  }
+      break;
+
+        case squ[2]:
+      if(this.state.checked==="X"){
+        squ[2]="X";
+        this.setState({checked:"O"})
+      }  else{
+        squ[2]="O";
+      this.setState({checked:"X"})  }
+     
+          break;
+      
+    default:
+     this.setState({
+       cells:"y",
+       checked:"z"
+     })
+  }
+}
+*//*
+handleclick=(event,index)=>{event.preventDefault()
+  this.setState({cells:"yes"})}
+*/
 // increment index
 // cant use switch because it will be speific so we using if statement
 //switch inside if
@@ -95,12 +148,13 @@ handleclick=(new,i)=>{
    render() {
     return (
       <div className="Appdiv">
+        
    {this.state.cells.map((item,i)=>{
       
-      return<li  onClick={this.handleclick} key={i} index={i}>{item}</li>
+      return<button  onClick={this.handleclick} key={i} index={i}>{item}</button>
     
      })} 
-   <ul>{this.state.cells}</ul>
+
    </div>
     );
    }}
